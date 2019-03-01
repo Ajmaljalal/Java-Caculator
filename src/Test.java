49e7ea7976099e36918e667f743ee47cc3fff7e7
Ajmal Jalal <ajmaljalal58@gmail.com>
2019-03-01 00:00:00 -0800

    project initialized

---
+++ src/Test.java
@@ -0,0 +1,16 @@
+
+import javax.swing.JFrame;
+
+
+public class Test {
+    public static void main( String args[] )
+   {
+      Calculator buttonFrame = new Calculator(); // create Button
+      buttonFrame.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
+      buttonFrame.setSize(230, 250); // set frame size
+      buttonFrame.setVisible( true ); // display frame
+       
+   } // end main
+} // end class 
+
+
---
+++ src/calculator/StartingPoint.java
@@ -0,0 +1,12 @@
+package calculator;
+
+import java.util.Scanner;
+
+	public class StartingPoint {
+
+		public static void main(String[] args) {
+		System.out.println("My First Programme");
+	}
+
+}
+
